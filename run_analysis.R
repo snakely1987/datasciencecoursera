8a01eaf4c8de22a59aa1f07fa0db413b988a9b58
snakely1987 <snakely1987@163.com>
2018-08-11 14:18:12 +0800

    注释语句

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,18 @@
-#Getting and Cleaning Data Course Project
 #1.Merges the training and the test sets to create one data set.
 TrainingSet=read.table("UCI HAR Dataset/train/X_train.txt")
 TestSet=read.table("UCI HAR Dataset/test/X_test.txt")
 mergedataset=rbind(TrainingSet,TestSet) #merge train and test dataset
 
+
+
+
 #2.Extracts only the measurements on the mean and standard deviation for each measurement.
 library(dplyr)
 feature=read.table("UCI HAR Dataset/features.txt")
 meanandstd=mergedataset[,grep("mean|std",feature[,2])]
+
+
+
 
 #3.Uses descriptive activity names to name the activities in the data set
 TrainingLabels=read.table("UCI HAR Dataset/train/y_train.txt")
@@ -16,8 +21,15 @@ mergedatalables=rbind(TrainingLabels,TestLabels)#merge train and test activity l
 Activity_Labels=factor(mergedatalables[,1],labels = c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING") )
 mergedataset=cbind(Activity_Labels,mergedataset)
 
+
+
+
 #4.Appropriately labels the data set with descriptive variable names.
 names(mergedataset)[2:ncol(mergedataset)]=as.character(feature[,2])
+
+
+
+
 
 #5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 splitbyactivity=split(mergedataset,mergedataset[,1])
@@ -29,7 +41,6 @@ for (i in 1:6)
 }
 meanbyact=meanbyact[,-1]
 rownames(meanbyact)=c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
-
 
 
 
